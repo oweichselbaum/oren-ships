a07ad855a06b9fe82e061af241a15aa417d50fbf
oweichselbaum <oren.weichselbaum@gmail.com>
2016-08-01 10:53:06 -0400

    Update product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -16,14 +16,6 @@ class Product
   validates_uniqueness_of :name
   validates_numericality_of :length, :width, :height, :weight
 
-  # scope :measurement_length, -> { where(:length.gte => measurement_length).order_by(length: :asc) }
-  # scope :measurement_width, -> { where(:width.gte => measurement_width).order_by(width: :asc) }
-  # scope :measurement_height, -> { where(:height.gte => measurement_height).order_by(height: :asc) }
-  # scope :measurement_weight, -> { where(:weight.gte => measurement_weight).order_by(weight: :asc) }
-  #
-  # def self.measurement_match length, width, height, weight
-  #   self.measurement_length(length).measurement_width(width).measurement_height(height).measurement_weight(weight).all
-  # end
   scope :product_length, ->(l) { where(:length.gte => l).order_by(length: :asc) }
   scope :product_width, ->(w) { where(:width.gte => w).order_by(width: :asc) }
   scope :product_height, ->(h) { where(:height.gte => h).order_by(height: :asc) }
